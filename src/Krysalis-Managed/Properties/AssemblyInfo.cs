7e4f06123c6d4a3f382c2e9c1d1cd5e136ffe501
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-10-16 12:31:33 -0500

    remove vs deps

--- src/Krysalis-Managed/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Krysalis")]
-[assembly: AssemblyProduct("Krysalis")]
-[assembly: AssemblyDescription("A prerequisite to a butterfly.")]
-[assembly: AssemblyCompany("DoubleStyx")]
-[assembly: AssemblyCopyright("Copyright © 2024 DoubleStyx")]
-[assembly: AssemblyVersion(Krysalis.Krysalis.VERSION_CONSTANT)]
-[assembly: AssemblyFileVersion(Krysalis.Krysalis.VERSION_CONSTANT)]
