9b7f70d11855096e40f4200b9aae6da8ca8234b8
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-10-16 10:52:04 -0500

    change template

--- ExampleMod/Properties/AssemblyInfo.cs
+++ ExampleMod/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ExampleMod")]
-[assembly: AssemblyProduct("ExampleMod")]
+[assembly: AssemblyTitle("Krysalis")]
+[assembly: AssemblyProduct("Krysalis")]
 [assembly: AssemblyDescription("Description of your mod")]
 [assembly: AssemblyCompany("ExampleAuthor")]
 [assembly: AssemblyCopyright("Copyright © 2024 ExampleAuthor")]
-[assembly: AssemblyVersion(ExampleMod.ExampleMod.VERSION_CONSTANT)]
-[assembly: AssemblyFileVersion(ExampleMod.ExampleMod.VERSION_CONSTANT)]
+[assembly: AssemblyVersion(Krysalis.Krysalis.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(Krysalis.Krysalis.VERSION_CONSTANT)]
