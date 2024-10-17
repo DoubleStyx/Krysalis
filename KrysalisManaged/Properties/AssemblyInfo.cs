c8a4946f2878b11ca320b7af7d75fad4f412bb40
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-10-17 14:24:26 -0500

    Revert "switch to just cmake"
    This reverts commit b442acd1f71e188181ad2906f79b299e94ac090c.

---
+++ KrysalisManaged/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("KrysalisManaged")]
+[assembly: AssemblyProduct("KrysalisManaged")]
+[assembly: AssemblyDescription("Description of your mod")]
+[assembly: AssemblyCompany("DoubleStyx")]
+[assembly: AssemblyCopyright("Copyright © 2024 DoubleStyx")]
+[assembly: AssemblyVersion(KrysalisManaged.KrysalisManaged.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(KrysalisManaged.KrysalisManaged.VERSION_CONSTANT)]
