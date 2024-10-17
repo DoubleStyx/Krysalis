e85665df05632fc05e43c3dd75af87b0d4d193d5
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-10-17 15:13:03 -0500

    Update KrysalisManaged.cs

--- KrysalisManaged/KrysalisManaged.cs
+++ KrysalisManaged/KrysalisManaged.cs
@@ -3,16 +3,16 @@ using HarmonyLib;
 using ResoniteModLoader;
 
 namespace KrysalisManaged;
-//More info on creating mods can be found https://github.com/DoubleStyx/ResoniteModLoader/wiki/Creating-Mods
+
 public class KrysalisManaged : ResoniteMod {
-	internal const string VERSION_CONSTANT = "1.0.0"; //Changing the version here updates it in all locations needed
-	public override string Name => "KrysalisManaged";
+	internal const string VERSION_CONSTANT = "1.0.0";
+	public override string Name => "Krysalis";
 	public override string Author => "DoubleStyx";
 	public override string Version => VERSION_CONSTANT;
-	public override string Link => "https://github.com/DoubleStyx/KrysalisManaged/";
+	public override string Link => "https://github.com/DoubleStyx/Krysalis/";
 
 	public override void OnEngineInit() {
-		Harmony harmony = new Harmony("net.DoubleStyx.KrysalisManaged");
+		Harmony harmony = new Harmony("net.DoubleStyx.Krysalis");
 		harmony.PatchAll();
 	}
 
