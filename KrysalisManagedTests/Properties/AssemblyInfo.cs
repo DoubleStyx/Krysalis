30a6f6e824dc7aca9b79b457168bf6d3ac68bdb5
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-10-18 15:40:02 -0500

    add unit test projects, adjust ci

---
+++ KrysalisManagedTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KrysalisManagedTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Razer")]
+[assembly: AssemblyProduct("KrysalisManagedTests")]
+[assembly: AssemblyCopyright("Copyright © Razer 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5db78969-b765-4bb6-84fd-a86fd385b323")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KrysalisNativeTests/KrysalisNativeTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace KrysalisNativeTests
+{
+	TEST_CLASS(KrysalisNativeTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
