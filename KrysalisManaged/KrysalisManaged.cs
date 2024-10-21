c00021dc947cc9020c1fb48fd7b048e1c49033b2
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-10-21 05:20:15 -0500

    rml_mods copying and fix ci

--- KrysalisManaged/KrysalisManaged.cs
+++ KrysalisManaged/KrysalisManaged.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KrysalisManaged
+namespace KrysalisManaged;
+
+public class KrysalisManaged
 {
-    public class KrysalisManaged
-    {
-    }
 }
